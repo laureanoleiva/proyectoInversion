b4ebab46975fad1096fec8bcd2c54d2e784f6036
Laureano Leiva <laureano.leiva.11@gmail.com>
2022-10-18 19:20:33 -0300

    Trabajo Practico 2

---
+++ activos.js
@@ -0,0 +1,55 @@
+const acciones = [
+    {
+        id: 1,
+        ticker: "AMZN",
+        name: "Amazon",
+        sector: "Tecnológico",
+        price: 114,
+        image: "./imagenes/amazon.png"
+    },
+    {
+        
+        id: 2,
+        ticker: "APPL",
+        name: "Apple",
+        sector: "Tecnológico",
+        price: 142,
+        image: "./imagenes/apple.png"
+    },
+    {
+        id: 3,
+        ticker: "GOOGL",
+        name: "Alphabet Inc.",
+        sector: "Tecnológico",
+        price: 100,
+        image: "./imagenes/google.png"
+    },
+    /*{
+        id: 4,
+        ticker: "KO",
+        name: "Coca-Cola Co.",
+        sector: "Industrial",
+        price: 56,
+        image: "./imagenes/cocacola.png"
+    },
+    {
+        id: 5,
+        ticker: "V",
+        name: "Visa Inc.",
+        sector: "Tecnológico",
+        price: 185,
+        image: "./imagenes/visa.jpg"
+    }*/
+]
+
+function Criptomonedas (id, ticker, name, price){
+    this.id=id;
+    this.ticker=ticker;
+    this.name=name;
+    this.price=price;
+}
+
+const crypto = [
+    crypto1 = new Criptomonedas(1,"BTC","Bitcoin",20000),
+    crypto2 = new Criptomonedas(2,"ETH","Ethereum",1300)
+]
--- java.js
+++ java.js
@@ -6,6 +6,8 @@ alert("¡¡Bienvenido, " + usuario + " !!")
 //ELECCIÓN DE TIPO DE INVERSIÓN A REALIZAR
 let inversion = parseInt(prompt("Elija tipo de inversión a realizar:\n1-Acciones\n2-Criptomonedas\n3-Obligaciones Negociables\n4-Bonos\n5-Fondo Común de Inversión\n6-Salir del Sistema"))
 //FUNCIÓN QUE INDIQUE QUE DESEA COMPRAR CON CICLO - A FUTURO EXISTIRÁ UNA LISTA DE LA CUAL PODRA SELECCIONAR EL ACTIVO
+
+
 function comprar() {
     let compraInversion;
     let cantidadInversion;
@@ -19,15 +21,21 @@ function comprar() {
         }
     } while (compraInversion != "fin");
 }
-
-
+//acciones.forEach((accion)=>alert(accion.id+" - "+accion.name));
+//alert("Elige entre las siguientes acciones:\n"+acciones.id+" - "+acciones.name+" - "+acciones.price)
 switch (inversion) {
     case 1:
-        alert("Comprarás Acciones");
+        alert("Hay " + acciones.length+ " Acciones disponibles para comprar:");
+        for (const accion of acciones) {
+            alert(accion.id + " - " + accion.name + " - Precio: $" + accion.price)
+        };
         comprar();
         break;
     case 2:
-        alert("Comprarás Criptomonedas");
+        alert("Hay " + crypto.length + " Criptomonedas disponibles para comprar:");
+        for (const cryptomoneda of crypto) {
+            alert(cryptomoneda.id + " - " + cryptomoneda.name + " - Precio: $" + cryptomoneda.price)
+        };
         comprar();
         break;
     case 3:
